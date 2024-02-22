ee1fb1f9f802e54b868b7a107a01fadaeb9747c7
HOME\Дом <angiy.kaa555@yandex.ru>
2024-02-22 20:02:05 +0300

    Fixed the toy class

--- Toy.java
+++ Toy.java
@@ -3,15 +3,31 @@
  */
 public class Toy {
 
-    int id;
-    String name;
-    int quantity;
-    float chanceOfDrop;
+    private int id;
+    private String name;
+    private int chanceOfDrop;
 
-    public Toy(int id, String name, int quantity, float chanceOfDrop){
+
+    public Toy(int id, String name, int chanceOfDrop){
         this.id = id;
         this.name = name;
-        this.quantity = quantity;
         this.chanceOfDrop = chanceOfDrop;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChanceOfDrop() {
+        return chanceOfDrop;
+    }
+
+    @Override
+    public String toString() {
+        return "Игрушка ID №" + id + ". " + name;
     }
 }
