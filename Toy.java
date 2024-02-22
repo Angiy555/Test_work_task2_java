95f359f3b3968a40591c8864ef298b181cff0397
HOME\Дом <angiy.kaa555@yandex.ru>
2024-02-22 18:13:00 +0300

    Adding quantity field

--- Toy.java
+++ Toy.java
@@ -5,11 +5,13 @@ public class Toy {
 
     int id;
     String name;
+    int quantity;
     float chanceOfDrop;
 
-    public Toy(int id, String name, float chanceOfDrop){
+    public Toy(int id, String name, int quantity, float chanceOfDrop){
         this.id = id;
         this.name = name;
+        this.quantity = quantity;
         this.chanceOfDrop = chanceOfDrop;
     }
 }
